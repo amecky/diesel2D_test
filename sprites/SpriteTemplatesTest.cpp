1965afccc5e1b2fd364c88784e292e38653faa26
meckya <amecky@gmail.com>
2016-01-12 21:51:16 +0100

    extending test

--- sprites/SpriteTemplatesTest.cpp
+++ sprites/SpriteTemplatesTest.cpp
@@ -9,6 +9,12 @@ TEST_CASE("BasicSpriteTemplatesTest", "[SpriteTemplates]") {
 	ds::Sprite& sp = s.get(0);
 	REQUIRE(sp.position.x == 512.0f);
 	REQUIRE(sp.position.y == 384.0f);
+	ds::Rect r = sp.texture.rect;
+	// 0,512,512,384
+	REQUIRE(r.top == 0);
+	REQUIRE(r.left == 512);
+	REQUIRE(r.width() == 512);
+	REQUIRE(r.height() == 384);
 }
 /*
 	void test_save_templates() {
